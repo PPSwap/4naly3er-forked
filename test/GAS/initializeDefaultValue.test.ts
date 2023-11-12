34d3a60f1809f5d23463f3c6ab70411ab0055f8c
Anik Tahabilder <tahabilderanik@gmail.com>
2023-11-11 21:40:44 -0500

    Added Dont initialize variable with default value

---
+++ test/GAS/initializeDefaultValue.test.ts
@@ -0,0 +1,3 @@
+import main from '../base';
+
+main("Don't initialize variables with default value", 'initializeDefaultValue.sol');
---
+++ test/contracts/initializeDefaultValue.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+contract InitializeDefaultValueTest {
+    uint  myNumber = 0;
+    bool isCorrect = false;
+
+
+}
