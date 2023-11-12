f7a8182189b7a437ced0660e12d9cb68cb0e9bb4
Anik Tahabilder <tahabilderanik@gmail.com>
2023-11-12 15:40:27 -0500

    Added DepricatedChainlinkFunciton use example file and test script

---
+++ test/M/deprecatedChainlinkFunction.test.ts
@@ -0,0 +1,3 @@
+import main from '../base';
+
+main('Use of deprecated chainlink function: `latestAnswer()`', 'deprecatedChainlinkFunction.sol');
---
+++ test/contracts/deprecatedChainlinkFunction.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract PriceConsumerSimple {
+    AggregatorV3Interface public priceFeed;
+
+    constructor(address _priceFeedAddress) {
+        priceFeed = AggregatorV3Interface(_priceFeedAddress);
+    }
+
+    function getLatestPrice() external view returns (int) {
+        return int(priceFeed.latestAnswer());
+    }
+}
