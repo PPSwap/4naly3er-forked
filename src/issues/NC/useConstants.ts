7c47b43bd225e0836f0925171f4aca95921608cf
imranpollob <polboy777@gmail.com>
2023-11-17 01:22:09 -0500

    remove nonReentrantBeforeModifiers test as the regex is not correcct

--- src/issues/NC/useConstants.ts
+++ src/issues/NC/useConstants.ts
@@ -6,10 +6,7 @@ const issue: RegexIssue = {
   title:'Constants should be defined rather than using magic numbers',
   regex:  /[0-9]{2,}?/g,
   /**
-   match occurrences where a number greater than or equal to 100 (excluding specific numbers like 10, 1e, 32, 256, and 128) appears in a context that is not a declaration of uint, int, or public.
-   
-   Example:
-   uint256 constant MAX_AMOUNT = 1000;
+   match occurrences where a number greater than or equal to 100
    */
 };
 
--- test/NC/_nonReentrantBeforeModifiers.test.ts
+++
@@ -1,4 +0,0 @@
-import main from '../base';
-
-// not working
-main('The `nonReentrant` `modifier` should occur before all other modifiers', 'nonReentrantBeforeModifiers.sol');
